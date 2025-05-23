7da2c520c5ac87cc34c661b3ff961289cd0ffd43
Louis Castricato <wirezapp@gmail.com>
2025-05-23 00:49:18 -0400

    workflow

--- reset-consent.sh
+++ reset-consent.sh
@@ -7,4 +7,4 @@ SCRIPT_DIR=$(dirname "$0")
 # Run the JS script
 node "$SCRIPT_DIR/reset-consent.js"
 
-echo "Script execution complete. Restart the VG Control app now."
+echo "Script execution complete. Restart the OWL Control app now."
--- vg_control/data/input_utils/README.md
+++ vg_control/data/input_utils/README.md
@@ -1,3 +1,3 @@
 # Input Utils
 
-Various utilities for parsing the CSV files created by VG Control
+Various utilities for parsing the CSV files created by OWL Control
