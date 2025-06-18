8490759810dda6a157927c36618aeccba81a6db0
Daniel Pechersky <danny.pechersky@gmail.com>
2025-06-18 17:38:53 -0400

    Strip brace brackets off hardware ID

--- crates/owl-recorder/src/hardware_id.rs
+++ crates/owl-recorder/src/hardware_id.rs
@@ -2,5 +2,6 @@ use color_eyre::Result;
 use game_process::hardware_id;
 
 pub(crate) fn get() -> Result<String> {
-    hardware_id()
+    // Strip "{}" off the ends of the windows hardware ID
+    hardware_id().map(|id| id[1..id.len() - 1].to_owned())
 }
